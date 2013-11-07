96fcccbafcea72eb9385c6e42f5dbada32058135
xxyy <xxyy@users.noreply.github.com>
2013-11-07 19:04:37 +0100

    CHnaged class name of App to ATDFPlugin.

--- src/main/java/io/github/xxyy/autotoggeldf/App.java
+++ src/main/java/io/github/xxyy/autotoggeldf/App.java
@@ -10,23 +10,23 @@ import org.bukkit.event.weather.WeatherChangeEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
- * Hai wurld!!1
+ * Main class for AutoToggelDownfall. Does stuff.
  *
  */
-public final class App extends JavaPlugin implements Listener {
+public final class ATDFPlugin extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
-        for (World wrld : Bukkit.getWorlds()) {
-            wrld.setStorm(false);
+        for (World wrld : Bukkit.getWorlds()) { //It might be raining on enable
+            wrld.setStorm(false); 
             wrld.setThundering(false);
         }
 
-        Bukkit.getPluginManager().registerEvents(this, this);
+        Bukkit.getPluginManager().registerEvents(this, this); //Register afterwards - setStorm(boolean) triggers the event.
     }
 
     @EventHandler(priority = EventPriority.LOW)
     public void onWeatherChange(final WeatherChangeEvent evt) {
-        evt.setCancelled(true);
+        evt.setCancelled(true); //Let's do this instead of just setting the weather to sun. Why? I donÄt know.
         evt.getWorld().setWeatherDuration(Integer.MAX_VALUE); //Duration of the CURRENT weather
         CommandHelper.broadcast("\u00a77\u00a7o{\u00a7eATDf: \u00a77\u00a7oSilenced da weather (atdf.msg)}", "atdf.msg");
     }
