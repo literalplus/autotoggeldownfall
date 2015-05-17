5fef52e4c255e0e9900324ba259169452a2f0a28
xxyy <devnull@nowak-at.net>
2015-05-17 21:45:38 +0200

    Improve code style in ATDFPlugin.java

--- src/main/java/io/github/xxyy/autotoggeldf/ATDFPlugin.java
+++ src/main/java/io/github/xxyy/autotoggeldf/ATDFPlugin.java
@@ -1,6 +1,5 @@
 package io.github.xxyy.autotoggeldf;
 
-import io.github.xxyy.common.util.CommandHelper;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.event.EventHandler;
@@ -9,26 +8,29 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.weather.WeatherChangeEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import io.github.xxyy.common.util.CommandHelper;
+
 /**
- * Main class for AutoToggelDownfall. Does stuff.
+ * Main class for AutoToggelDownfall, providing an interface from the plugin logic to the Bukkit API.
  *
+ * @author xxyy
  */
 public final class ATDFPlugin extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        for (World wrld : Bukkit.getWorlds()) { //It might be raining on enable
-            wrld.setStorm(false);
-            wrld.setThundering(false);
+        for (World world : Bukkit.getWorlds()) { //It might be raining on enable
+            world.setStorm(false);
+            world.setThundering(false);
         }
 
-        Bukkit.getPluginManager().registerEvents(this, this); //Register afterwards - setStorm(boolean) triggers the event.
+        Bukkit.getPluginManager().registerEvents(this, this);
     }
 
     @EventHandler(priority = EventPriority.LOW)
     public void onWeatherChange(final WeatherChangeEvent evt) {
-        evt.setCancelled(true); //Let's do this instead of just setting the weather to sun. Why? I donÄt know.
-        evt.getWorld().setWeatherDuration(Integer.MAX_VALUE); //Duration of the CURRENT weather
+        evt.setCancelled(true); //imagine if the server admin had an event or something and wants to keep rain
+        evt.getWorld().setWeatherDuration(Integer.MAX_VALUE);
         CommandHelper.broadcast("\u00a77\u00a7o{\u00a7eATDf: \u00a77\u00a7oSilenced da weather (atdf.msg)}", "atdf.msg");
     }
 }
